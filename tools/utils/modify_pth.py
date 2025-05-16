03862ca8aadbb99b5e7a77be7fe911abc39e34da
jiangxb98 <1587884826@qq.com>
2025-05-16 15:03:45 +0800

    update wo gt config

--- tools/utils/modify_pth.py
+++ tools/utils/modify_pth.py
@@ -6,18 +6,23 @@ import torch
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--pth_path", type=str, default="model.pth")
+    parser.add_argument("--pth_path", type=str, default="pretrained/plan_epoch_6_ft.pth")
     # key is the list for the keys to be deleted
-    parser.add_argument("--key", nargs="+", type=str, default=['plan_head'])
+    parser.add_argument("--key", nargs="+", type=str, default=['plan_head'])  # --key plan_head palnesadas
 
     args = parser.parse_args()
 
     # 读取pth文件
     state_dict = torch.load(args.pth_path)
-
+    
     # 删除指定key的值
-    for key in args.key:
-        del state_dict[key]
+    keys = list(state_dict['state_dict'].keys())
+    for del_key in args.key:
+        print('del the key is {}'.format(del_key))
+        for key in keys:
+            if del_key in key:
+                del state_dict['state_dict'][key]
+                print("del : {}".format(key))
 
     # 保存到新的pth文件中
     output_path = args.pth_path.replace(".pth", "_modified.pth")
